9c197e28464c513f8b87bc394bfc8cca4f7da4e2
Jayathilaka_Sagara <sagara.jayathilaka@pearson.com>
2023-08-22 22:59:44 +0530

    Shell script

--- ansible/roles/k8s-master/vars/install-containerd.sh
+++ ansible/roles/k8s-master/vars/install-containerd.sh
@@ -1,45 +1,10 @@
 ###########################################
-# Forwarding IPv4 and letting iptables see bridged traffic
-###########################################
-echo "2. Forwarding IPv4"
-cp /home/ubuntu/ansible_files/k8s.conf /etc/modules-load.d/k8s.conf
-
-modprobe overlay
-modprobe br_netfilter
-
-# sysctl params required by setup, params persist across reboots
-cp /home/ubuntu/ansible_files/1k8s.conf /etc/sysctl.d/k8s.conf
-
-
-
-# Apply sysctl params without reboot
-sysctl --system
-
-sysctl net.bridge.bridge-nf-call-iptables net.bridge.bridge-nf-call-ip6tables net.ipv4.ip_forward
-
-
-###########################################
 # Insytall containerd
 ###########################################
 echo "3. Set up repository"
 #Set up the repository
-#Update the apt package index and install packages to allow apt to use a repository over HTTPS:
-apt-get update
-apt-get install -y ca-certificates curl gnupg
-#Add Docker’s official GPG key:
-install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 chmod a+r /etc/apt/keyrings/docker.gpg
 #Use the following command to set up the repository:
 echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-# Install containerd
-echo "4. Install ContainerD"
-apt-get install -y containerd.io
 
-
-#### Container runtime configuration setup
-#sudo vi /etc/containerd/config.toml
-echo "5. Create /etc/containerd/config.toml"
-mkdir -p /etc/containerd
-echo "6. Set containerd toml config"
-containerd config default > /etc/containerd/config.toml
