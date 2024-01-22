0d9e9ddf2a8dfc8fc7ba4ba9165a869248757ed0
Jayathilaka_Sagara <sagara.jayathilaka@pearson.com>
2024-01-22 12:20:16 +0530

    install ansible ubuntu script

---
+++ ansible-general/ansible_install_ubuntu.sh
@@ -0,0 +1,6 @@
+## Install Ansible in Ubuntu
+#! /bin/bash
+sudo apt update
+sudo apt install software-properties-common
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible
