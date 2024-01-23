9a0745bfb95ee3edea4969afc63368c8757e6e18
Jayathilaka_Sagara <sagara.jayathilaka@pearson.com>
2024-01-23 17:41:20 +0530

    master check

---
+++ ansible/roles/k8s_master/files/check_if_master.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if pgrep -f kube-apiserver >/dev/null && pgrep -f kube-controller-manager >/dev/null && pgrep -f kube-scheduler >/dev/null; then
+    echo "This server is a Kubernetes master."
+    export K8S_MASTER= true
+else
+    echo "This server is not a Kubernetes master."
+    export K8S_MASTER= false
+fi
--- ansible/roles/k8s_master/files/cluster.sh
+++ ansible/roles/k8s_master/files/cluster.sh
@@ -1,5 +1,7 @@
 ## Initialize Kubernetes Cluster
 #!/bin/bash
-
-kubeadm init --pod-network-cidr=$1 --apiserver-advertise-address=$2
+# Onlt initialze the Kubernetest cluster of the server is not already a MAster
+if not $K8S_MASTER; then
+    kubeadm init --pod-network-cidr=$1 --apiserver-advertise-address=$2
+fi
 #kubeadm init --pod-network-cidr=10.0.0.0/24 --apiserver-advertise-address=10.0.2.71
