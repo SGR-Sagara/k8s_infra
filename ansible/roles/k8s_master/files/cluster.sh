15e4141b1ca24cc6a6251a6322409ce533b40a60
Jayathilaka_Sagara <sagara.jayathilaka@pearson.com>
2024-01-23 18:36:09 +0530

    echo "Server is already a Master"

--- ansible/roles/k8s_master/files/cluster.sh
+++ ansible/roles/k8s_master/files/cluster.sh
@@ -1,7 +1,9 @@
 ## Initialize Kubernetes Cluster
 #!/bin/bash
 # Onlt initialze the Kubernetest cluster of the server is not already a MAster
-if not $K8S_MASTER; then
+if [ "$K8S_MASTER" = "false" ]; then
     kubeadm init --pod-network-cidr=$1 --apiserver-advertise-address=$2
+else
+    echo "Server is already a Master"
 fi
 #kubeadm init --pod-network-cidr=10.0.0.0/24 --apiserver-advertise-address=10.0.2.71
