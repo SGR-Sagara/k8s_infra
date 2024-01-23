4f1cb65d1d4c8073a09043425905bd3234d25c01
Jayathilaka_Sagara <sagara.jayathilaka@pearson.com>
2024-01-23 19:34:22 +0530

    new try

--- ansible/roles/k8s_master/files/check_if_master.sh
+++ ansible/roles/k8s_master/files/check_if_master.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+## Check whther kubernetes master cluster related services are running if not initialize the cluster
 if pgrep -f kube-apiserver >/dev/null && pgrep -f kube-controller-manager >/dev/null && pgrep -f kube-scheduler >/dev/null; then
     echo "This server is a Kubernetes master."
-    export K8S_MASTER="true"
 else
     echo "This server is not a Kubernetes master."
-    export K8S_MASTER="false"
+    kubeadm init --pod-network-cidr=$1 --apiserver-advertise-address=$2
 fi
